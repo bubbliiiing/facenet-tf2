94e1ece8ce1a0f18aaaee993561864e0f5e7a926
Bubbliiiing <3323290568@qq.com>
2022-03-18 18:30:32 +0800

    update a lot

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,8 +1,10 @@
 import os
 import warnings
 
-import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 import scipy.signal
 from tensorflow import keras
 from tensorflow.keras import backend as K
@@ -12,24 +14,23 @@ from utils.utils_metrics import evaluate
 
 class LossHistory(keras.callbacks.Callback):
     def __init__(self, log_dir):
-        import datetime
-        curr_time = datetime.datetime.now()
-        time_str = datetime.datetime.strftime(curr_time,'%Y_%m_%d_%H_%M_%S')
         self.log_dir    = log_dir
-        self.time_str   = time_str
-        self.save_path  = os.path.join(self.log_dir, "loss_" + str(self.time_str))  
         self.losses     = []
         self.val_loss   = []
         
-        os.makedirs(self.save_path)
+        os.makedirs(self.log_dir)
 
-    def on_epoch_end(self, batch, logs={}):
+    def on_epoch_end(self, epoch, logs={}):
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.save_path)
+
         self.losses.append(logs.get('loss'))
         self.val_loss.append(logs.get('val_loss'))
-        with open(os.path.join(self.save_path, "epoch_loss_" + str(self.time_str) + ".txt"), 'a') as f:
+        
+        with open(os.path.join(self.log_dir, "epoch_loss.txt"), 'a') as f:
             f.write(str(logs.get('loss')))
             f.write("\n")
-        with open(os.path.join(self.save_path, "epoch_val_loss_" + str(self.time_str) + ".txt"), 'a') as f:
+        with open(os.path.join(self.log_dir, "epoch_val_loss.txt"), 'a') as f:
             f.write(str(logs.get('val_loss')))
             f.write("\n")
         self.loss_plot()
@@ -57,7 +58,7 @@ class LossHistory(keras.callbacks.Callback):
         plt.title('A Loss Curve')
         plt.legend(loc="upper right")
 
-        plt.savefig(os.path.join(self.save_path, "epoch_loss_" + str(self.time_str) + ".png"))
+        plt.savefig(os.path.join(self.log_dir, "epoch_loss.png"))
 
         plt.cla()
         plt.close("all")
@@ -75,7 +76,7 @@ class ExponentDecayScheduler(keras.callbacks.Callback):
         learning_rate = K.get_value(self.model.optimizer.lr) * self.decay_rate
         K.set_value(self.model.optimizer.lr, learning_rate)
         if self.verbose > 0:
-            print('\nSetting learning rate to %s.' % (learning_rate))
+            print('Setting learning rate to %s.' % (learning_rate))
 
 class LFW_callback(keras.callbacks.Callback):
     def __init__(self, test_loader):
@@ -95,14 +96,14 @@ class LFW_callback(keras.callbacks.Callback):
         print("正在进行LFW数据集测试")
 
         for _, (data_a, data_p, label) in enumerate(self.test_loader.generate()):
-            out_a, out_p = self.model.predict(data_a)[1], self.model.predict(data_p)[1]
-            dists = np.linalg.norm(out_a - out_p, axis=1)
+            out_a, out_p    = self.model.predict(data_a)[1], self.model.predict(data_p)[1]
+            dists           = np.linalg.norm(out_a - out_p, axis=1)
 
             distances.append(dists)
             labels.append(label)
 
-        labels = np.array([sublabel for label in labels for sublabel in label])
-        distances = np.array([subdist for dist in distances for subdist in dist])
+        labels      = np.array([sublabel for label in labels for sublabel in label])
+        distances   = np.array([subdist for dist in distances for subdist in dist])
         _, _, accuracy, _, _, _, _ = evaluate(distances,labels)
         print('Accuracy: %2.5f+-%2.5f' % (np.mean(accuracy), np.std(accuracy)))
  
